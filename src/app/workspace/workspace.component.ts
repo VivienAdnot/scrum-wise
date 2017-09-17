2d396a1fbdaf22cde2f5037b9db104f1935ee9a8
Adnot Vivien <vivien.adnot@gmail.com>
2017-09-17 12:48:08 +0200

    display current value of each slider

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -11,21 +11,28 @@ export class WorkspaceComponent implements OnInit {
     total: number;
     phases: PhaseModel[];
     sliderValues: number[];
+    sliderInputs: number[];
 
     constructor(private workspaceService: WorkspaceService) {
         this.sliderValues = [];
+        this.sliderInputs = [];
     }
 
     async ngOnInit() {
         this.phases = await this.workspaceService.getPhases();
 
         this.sliderValues = this.phases.map((phase) => phase.defaultValue);
+        this.sliderInputs = this.sliderValues;
         this.computeTotal();
     }
 
-    onChange(event, id) {
-        this.sliderValues[id] = event.value;
+    onChange(event, sliderId) {
+        this.sliderValues[sliderId] = event.value;
         this.computeTotal();
+    }
+
+    onInput(event, sliderId) {
+        this.sliderInputs[sliderId] = event.value;
     }
 
     computeTotal() {
